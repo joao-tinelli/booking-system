ee9f359dc16b26752474af68c871ca4929437574
WallaceJunior14 <wallacepfj1409@gmail.com>
2025-01-17 01:11:16 -0300

    AVL tree created

--- includes/avl.h
+++ includes/avl.h
@@ -1,3 +1,98 @@
 #ifndef H_AVL
 #define H_AVL
+
+typedef struct _node Node;
+
+/**
+ * @brief Funcao de criacao de um novo no
+ * @param code, capacity
+ * @return *node
+ */
+Node *newNode(unsigned int code, unsigned int capacity);
+
+/**
+ * @brief Retorna o maior codigo
+ * @param a, b
+ * @return a ou b
+ */
+short bigger(short a, short b);
+
+/**
+ * @brief Retorna a altura de um no ou -1, caso ele seja Null
+ * @param *node
+ * @return short (height);
+ */
+short nodeHeight(Node *node);
+
+/**
+ * @brief Calcula o fator de balanceamento
+ * @param *node
+ * @return short (fator de balanceamento)
+ */
+short balancingFactor(Node *node);
+
+/**
+ * @brief Funcao para realizar a rotacao a esquerda;
+ * @param *root
+ * @return y (nova root)
+ */
+Node *leftRotation(Node *root);
+
+/**
+ * @brief Funcao para realizar a rotacao a direita
+ * @param *root
+ * @return y (nova root)
+ */
+Node *rightRotation(Node *root);
+
+/**
+ * @brief Funcao para realizar a rotacao dupla (esquerda - direita)
+ * @param *root
+ * @return rotacao a esquerdaa e a direita
+ */
+Node* leftAndRightRotation(Node *root);
+
+/**
+ * @brief Funcao para realizar a rotacao dupla (esquerda - direita)
+ * @param *root
+ * @return rotacao a esquerdaa e a direita
+ */
+Node* leftAndRightRotation(Node *root);
+
+/**
+ * @brief Funcao para realizar a rotacao dupla (direita - esquerda)
+ * @param *root
+ * @return rotacao a esquerda e a direita
+ */
+Node* rightAndLeftRotation(Node *root);
+
+/**
+ * @brief Funcao para realizar o balanceamento apos uma insercao ou remocao
+ * @param *root
+ * @return *root
+ */
+Node *balance(Node *root);
+
+/**
+ * @brief Insere um novo nó na arvore
+ * @param *root, code, capacity
+ * @return *root
+ */
+Node *insertRoom(Node *root, unsigned int code, unsigned int capacity);
+
+/**
+ * @brief Funcao para remover um no da arvore
+ * @param *root, code
+ * @return *root
+ */
+Node *removeRoom(Node *root, unsigned int code);
+
+/**
+ * @brief Funcao para mostrar a arvore avl
+ * @param *root, level
+ * @return void
+ */
+void showAVLTree(Node *root, int level);
+
+
 #endif
--- src/avl.c
+++ src/avl.c
@@ -1,1 +1,194 @@
 #include "avl.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node {
+    unsigned int code, capacity;
+    struct _node *left, *right;
+    short height;
+} Node;
+
+Node *newNode(unsigned int code, unsigned int capacity) {
+    Node *node = (Node*) malloc(sizeof(Node));
+
+    if (node) {
+        node->code = code;
+        node->capacity = capacity;
+        node->left = node->right = NULL;
+        node->height = 0;
+    }
+    else 
+        perror("\nERRO ao alocar no em 'noNovo()'");
+    
+    return node;
+}
+
+short bigger(short a, short b) {
+    return (a > b) ? a : b;
+}
+
+short nodeHeight(Node *node) {
+    if (node == NULL) 
+        return -1;
+    else 
+        return node->height;
+}
+
+short balancingFactor(Node *node) {
+    if (node)
+        return (nodeHeight(node->left)) - (nodeHeight(node->right));
+    else    
+        return 0;
+}
+
+Node *leftRotation(Node *root) {
+    Node *y, *f; //f -> FILHO
+
+    y = root->right;
+    f = y->left;
+
+    y->left = root;
+    root->right = f; //f se torna o filho a direita de r (caso tenha)
+
+    root->height = bigger(nodeHeight(root->left), nodeHeight(root->right)) + 1;
+    y->height = bigger(nodeHeight(y->left), nodeHeight(y->right)) + 1;
+
+    return y;
+} 
+
+Node *rightRotation(Node *root) {
+    Node *y, *f; // f -> filho
+
+    y = root->left;
+    f = y->right;
+
+    y->right = root;
+    root->left = f; //f se torna o filho a esquerdo de r (caso tenha)
+
+    root->height = bigger(nodeHeight(root->left), nodeHeight(root->right)) + 1;
+    y->height = bigger(nodeHeight(y->left), nodeHeight(y->right)) + 1;
+
+    return y;
+} 
+
+Node* leftAndRightRotation(Node *root) {
+    root->left = leftRotation(root->left);
+    return rightRotation(root);
+}
+
+Node* rightAndLeftRotation(Node *root) {
+    root->right = rightRotation(root->right);
+    return leftRotation(root);
+}
+
+Node *balance(Node *root) {
+    short fb = balancingFactor(root);
+
+    // rotacao a esquerda
+    if (fb < -1 && balancingFactor(root->right) <= 0)
+        root = leftRotation(root);
+    // rotacao a direita
+    else if (fb > 1 && balancingFactor(root->left) >= 0)
+        root = rightRotation(root);
+    // Rotacao dupla -> esquerda e direita
+    else if (fb > 1 && balancingFactor(root->left) < 0)
+        root = leftAndRightRotation(root);
+    // Rotacao dupla -> direita e esquerda
+    else if (fb < -1 && balancingFactor(root->right) > 0)
+        root = rightAndLeftRotation(root);
+
+    return root;
+}
+
+Node *inseinsertRoomt(Node *root, unsigned int code, unsigned int capacity) {
+    // arvore vazia
+    if (root == NULL) 
+        return newNode(code, capacity);
+    // insercao sera para a esquerda ou direita
+    else {
+        if (code < root->code)
+            root->left = insertRoom(root->left, code, capacity);
+        else if (code > root->code)
+            root->right = insertRoom(root->right, code, capacity);
+        else
+            perror("\nInsercao nao realida!");
+    }
+
+    // Recalcula a altura de todos os nos entre a root e o novo no inserido
+    root->height = bigger(nodeHeight(root->left), nodeHeight(root->right)) + 1;
+
+    // verifica a necessidade de balancear a arvore
+    root = balance(root);
+    
+    return root;
+}
+
+Node *removeRoom(Node *root, unsigned int code) {
+    if (root == NULL) {
+        perror("code nao encontrado!\n");
+        return NULL;
+    }
+    // procura o no a remover
+    else {
+        if (root->code == code) {
+            // remove nos folhas (nos sem filhos)
+            if (root->left == NULL && root->right == NULL) {
+                free(root);
+                printf("Elemento folha removido: %d ! \n", code);
+                return NULL;
+            }
+            else {
+                // remover no que possi 2 filhos
+                if (root->left != NULL && root->right != NULL) {
+                    Node *aux = root->left;
+                    while (aux->right != NULL)
+                        aux = aux->right;
+                    root->code = aux->code;
+                    aux->code = code;
+                    printf("Elemento trocado: %d !\n", code);
+                    root->left = removeRoom(root->left, code);
+                    return root;
+                }
+                else {
+                    // remover nos que possuem apenas 1 filho
+                    Node *aux;
+                    if (root->left != NULL)
+                        aux = root->left;
+                    else 
+                        aux = root->right;
+                    free(root);
+                    printf("Elemento com 1 filho removido: %d\n", code);
+                    return aux;
+                }
+            }
+        } else  {
+            if (code < root->code)
+                root->left = removeRoom(root->left, code);
+            else 
+                root->right = removeRoom(root->right, code);
+        }
+
+        // Recalcula a altura de todos os nos entre a root e o novo no inserido
+        root->height = bigger(nodeHeight(root->left), nodeHeight(root->right)) + 1;
+
+        // verifica a necessidade de balancear a arvore
+        root = balance(root);
+        
+        return root;
+    }
+    
+}
+
+void showAVLTree(Node *root, int level) {
+    if (root) {
+        showAVLTree(root->right, level + 1);
+        printf("\n\n");
+
+        for (int  i = 0; i < level; i++)
+            printf("\t");
+        
+        printf("%d", root->code);
+        showAVLTree(root->left, level + 1);
+    }
+}
+
