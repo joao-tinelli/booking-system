139f4c082306245d13f1f4444fd3e489663b2e18
WallaceJunior14 <wallacepfj1409@gmail.com>
2025-01-18 16:57:08 -0300

    Hash Table create

--- includes/hash.h
+++ includes/hash.h
@@ -1,3 +1,85 @@
 #ifndef H_HASH
 #define H_HASH
+
+typedef struct _node NodeHash;
+
+typedef struct ListHash;
+
+/**
+ * @brief Função para inicializar a lista de cada elemento da tabela hash.
+ * @param *list
+ * @return (void)
+ */
+void inicializeList(ListHash *list);
+
+/**
+ * @brief Função para inserir na lista de uma posição específica da tabela hash.
+ * @param *list, code, *status
+ * @return (void)
+ */
+void insertIntoList(ListHash *list, unsigned int code, char *status);
+
+/**
+ * @brief Função para atualizar o status de uma sala dentro de uma lista de uma posição específica da tabela hash.
+ * @param *list, code, *newStatus
+ * @return (void)
+ */
+void updateRoomStatus(ListHash *list, unsigned int code, char *newStatus);
+
+/**
+ * @brief Função para buscar o status de uma sala dentro de uma lista de uma posição específica da tabela hash.
+ * @param *list, code
+ * @return status
+ */
+char *searchList(ListHash *list, unsigned int code);
+
+/**
+ * @brief Função para imprimir a lista de uma posição específica da tabela hash.
+ * @param *list
+ * @return (void)
+ */
+void printList(ListHash *list);
+
+/**
+ * @brief Função para inicializar a tabela hash.
+ * @param t[], size
+ * @return (void)
+ */
+void inicializeHashTable(ListHash t[], unsigned int size);
+
+/**
+ * @brief Função para gerar a posição na tabela hash.
+ * @param code, size
+ * @return id
+ */
+int hashFunction(unsigned int code, unsigned int size);
+
+/**
+ * @brief Função para inserir na tabela hash.
+ * @param t[], code, *status, size
+ * @return (void)
+ */
+void insertIntoHashTable(ListHash t[], unsigned int code, char *status, unsigned int size);
+
+/**
+ * @brief Função para atualizar o status de uma sala na tabela hash.
+ * @param t[], code, *status, size
+ * @return (void)
+ */
+void updateStatusIntoHashTable(ListHash t[], unsigned int code, char *newStatus, unsigned int size);
+
+/**
+ * @brief Função para buscar o status de uma sala dentro da tabela hash.
+ * @param t[], code, size
+ * @return *status
+ */
+char *searchStatusIntoHashTable(ListHash t[], unsigned int code, unsigned int size);
+
+/**
+ * @brief Função para imprimir a tabela hash.
+ * @param t[], size
+ * @return (void)
+ */
+void printHashTable(ListHash t[], const unsigned int size);
+
 #endif
--- src/hash.c
+++ src/hash.c
@@ -1,1 +1,103 @@
 #include "hash.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct _node {
+    unsigned int code;
+    char status[25];
+    struct _node *next;
+} NodeHash;
+
+typedef struct {
+    NodeHash *start;
+    unsigned int tam;
+} ListHash;
+
+void inicializeList(ListHash *list) {
+    list->start = NULL;
+    list->tam = 0;
+}
+
+void insertIntoList(ListHash *list, unsigned int code, char *status) {
+    NodeHash *node = (NodeHash*) malloc(sizeof(NodeHash));
+
+    if (node) {
+        node->code = code;
+        strcpy(node->status, status);
+        node->next = list->start;
+        list->start = node;
+        list->tam++;
+    } else {
+        perror("Error: Problema ao inserir na lista!\n");
+    }
+}
+
+void updateRoomStatus(ListHash *list, unsigned int code, char *newStatus) {
+    NodeHash *aux = list->start;
+
+    while (aux && aux->code != code) {
+        aux = aux->next;
+    }
+
+    if (aux) {
+        strcpy(aux->status, newStatus);
+        printf("\nSuccess: Status da sala %d atualizado!\n", code);
+    } else {
+        printf("\nERROR: Sala com código %d não encontrada!\n", code);
+    }
+}
+
+char *searchList(ListHash *list, unsigned int code) {
+    NodeHash *aux = list->start;
+
+    while (aux && aux->code != code) {
+        aux = aux->next;
+    }
+
+    return aux ? aux->status : NULL;
+}
+
+void printList(ListHash *list) {
+    NodeHash *aux = list->start;
+
+    while (aux) {
+        printf("[%d: %s] -> ", aux->code, aux->status);
+        aux = aux->next;
+    }
+    printf("NULL");
+}
+
+void inicializeHashTable(ListHash t[], unsigned int size) {
+    for (int i = 0; i < size; i++) {
+        inicializeList(&t[i]);
+    }
+}
+
+int hashFunction(unsigned int code, unsigned int size) {
+    return code % size;
+}
+
+void insertIntoHashTable(ListHash t[], unsigned int code, char *status, unsigned int size) {
+    int id = hashFunction(code, size);
+    insertIntoList(&t[id], code, status);
+}
+
+void updateStatusIntoHashTable(ListHash t[], unsigned int code, char *newStatus, unsigned int size) {
+    int id = hashFunction(code, size);
+    updateRoomStatus(&t[id], code, newStatus);
+}
+
+char *searchStatusIntoHashTable(ListHash t[], unsigned int code, unsigned int size) {
+    int id = hashFunction(code, size);
+    printf("-> Indice gerado: %d.\n", id);
+    return searchList(&t[id], code);
+}
+
+void printHashTable(ListHash t[], const unsigned int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d = ", i);
+        printList(&t[i]);
+        puts("");
+    }
+}
